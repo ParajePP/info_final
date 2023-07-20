e299983a91eb3e386c1f1e3d475f86646ae6ce83
Tami Lang Goy <parajeproduccionypantalla@gmail.com>
2023-07-19 22:37:20 -0300

    bd

--- app_info/app_info/settings.py
+++ app_info/app_info/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'app_info.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app_info',
+        'USER':'root2',
+        'PASSWORD': 'Cuidado.7',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
