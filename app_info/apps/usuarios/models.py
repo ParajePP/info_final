38174f95015847ab1f8da124ef3329c199548452
Tami Lang Goy <parajeproduccionypantalla@gmail.com>
2023-07-23 12:27:32 -0300

    models

--- app_info/apps/usuarios/models.py
+++ app_info/apps/usuarios/models.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to="images/usuarios/")
+    
+    
 class Usuario (AbstractUser):
     nombre = models.CharField(max_length=20, null=True)
     apellido= models.CharField(max_length=20, null=True)
     edad= models.CharField(max_length=2, null=True)
     is_staff= models.BooleanField( 'es_staff', default=False)
     imagen = models.ImageField(null=True, blank=True, upload_to='usuarios',default='usuarios/usuarios_def.png')
+    profile= models.ForeignKey(Profile)
     
     def __str__(self):
         return self.nombre 
