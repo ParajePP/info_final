81fba5f7531c0b42ec7bbd7853be29413cb220b8
Tami Lang Goy <parajeproduccionypantalla@gmail.com>
2023-07-27 18:03:15 -0300

    modifique el navbar para poner los permisos del ad

--- app_info/apps/usuarios/urls.py
+++ app_info/apps/usuarios/urls.py
@@ -1,12 +1,16 @@
 
+
+
 import statistics
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from app_info.views import inicio
 from .views import *
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 
     
 urlpatterns = [
@@ -14,7 +18,7 @@ urlpatterns = [
    path('iniciar_sesion/', LoginView.as_view(template_name = 'usuarios/iniciar_sesion.html'), name='iniciar_sesion'),
    path('cerrar_sesion/', LogoutView.as_view(), name='cerrar_sesion'),
    path('listar_usuarios/', ListarUsuarios, name='listar_usuarios'),
-   ]
+   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += statistics(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
