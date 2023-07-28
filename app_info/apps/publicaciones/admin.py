0ba58a3f4fda9be52ad2481bfdf4a7f1045845f5
Tami Lang Goy <parajeproduccionypantalla@gmail.com>
2023-07-28 11:18:30 -0300

    anda el nav no anda iniciar sesion

--- app_info/apps/publicaciones/admin.py
+++ app_info/apps/publicaciones/admin.py
@@ -3,7 +3,7 @@ from .models import Categoria, Publicaciones
 
 @admin.register(Publicaciones)
 class Publicaciones(admin.ModelAdmin):
-    list_display=('titulo','empresa','fecha','descripcion','categoria','imagen','colaborador')
+    list_display=('titulo','fecha','descripcion','categoria','imagen','colaborador')
 
 
 
