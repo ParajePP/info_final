77ab86658bceb5c986a67ea14453c359c37174c5
Juanjo_P <juan_pighin@hotmail.com>
2023-07-28 18:46:51 -0300

    corrección botones inicio y registrar

--- app_info/apps/usuarios/models.py
+++ app_info/apps/usuarios/models.py
@@ -12,7 +12,7 @@ class Usuario (AbstractUser):
     apellido= models.CharField(max_length=20, null=True)
     edad= models.CharField(max_length=2, null=True)
     is_staff= models.BooleanField( 'es_staff', default=False)
-    imagen = models.ImageField(null=True, blank=True, upload_to='usuarios',default='usuarios/usuarios_def.png')
+    imagen = models.ImageField(null=True, blank=True, upload_to='usuarios',default='usuarios/inicio_sesion.png')
     #profile= models.ForeignKey(Profile)
     
     def __str__(self):
